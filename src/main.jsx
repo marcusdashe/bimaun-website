bed8e3ad8bf39829f6a4995e8a0ae332d26890d1
Marcus Dashe <marcus.dashe@totalsecureng.com>
2024-05-23 03:09:15 +0100

    Fix: production mime error

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// add the beginning of your app entry
+import 'vite/modulepreload-polyfill'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
